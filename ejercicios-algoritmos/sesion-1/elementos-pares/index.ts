14062ceec88452e69ae707df4f55e5175990347f
hidetsumi <hidetsu99@gmail.com>
2025-01-02 14:49:23 -0300

    feat: elementos-pares completed fix: change toBe test to toEqual

--- ejercicios-algoritmos/sesion-1/elementos-pares/index.ts
+++ ejercicios-algoritmos/sesion-1/elementos-pares/index.ts
@@ -1,4 +1,25 @@
-export default function filtrarPares(array: unknown[]): unknown[] {
-  // TODO: implement
-  return [];
+interface OddMap {
+  [key: string | number]: number;
 }
+
+export default function filtrarPares<T extends string | number>(
+  array: T[]
+): T[] {
+  const oddsValues = array.reduce((map: OddMap, current) => {
+    map[current] = (map[current] || 0) + 1;
+    return map;
+  }, {});
+
+  return Object.entries(oddsValues).reduce((odds: T[], [key, value]) => {
+    if (value % 2 === 0) {
+      const parsedKey = !isNaN(Number(key)) ? Number(key) : key;
+
+      odds.push(parsedKey as T);
+    }
+    return odds;
+  }, []);
+}
+
+
+console.log(filtrarPares([1, 1, 2, 3, 4, 4, 5]));
+
