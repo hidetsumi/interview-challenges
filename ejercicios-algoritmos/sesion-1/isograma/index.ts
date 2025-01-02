b485fb7f5682a2cc46636bbd50427e924209a300
hidetsumi <hidetsu99@gmail.com>
2025-01-02 12:10:30 -0300

    feat: isograma completed

--- ejercicios-algoritmos/sesion-1/isograma/index.ts
+++ ejercicios-algoritmos/sesion-1/isograma/index.ts
@@ -1,4 +1,13 @@
 export default function esIsograma(string: string): boolean {
-  // TODO: implement
-  return false;
+  const moreThanTwoWords = string.split(" ")[1] ? true : false;
+
+  if (moreThanTwoWords) {
+    return false;
+  }
+
+  const formatedString = string.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  
+  
+  return new Set([...formatedString]).size === string.length
+
 }
