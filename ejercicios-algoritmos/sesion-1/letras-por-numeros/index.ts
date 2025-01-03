2cb4eebf3d6cc9410badefa42273bba006b0207a
hidetsumi <hidetsu99@gmail.com>
2025-01-03 12:40:01 -0300

    feat: letras por numeros completed

--- ejercicios-algoritmos/sesion-1/letras-por-numeros/index.ts
+++ ejercicios-algoritmos/sesion-1/letras-por-numeros/index.ts
@@ -1,4 +1,13 @@
+const MAP = [
+ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+ 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+ 'u', 'v', 'w', 'x', 'y', 'z'
+];
 export default function letrasPorNumeros(string: string): string {
-  // TODO: implement
-  return "";
+  const parsedString = string.replaceAll(/\s/g, "").toLowerCase();
+
+  return Array.from(parsedString).reduce(
+    (acc, letter) => acc.concat(`${MAP.indexOf(letter) + 1} `),
+    ""
+  ).trim();
 }
